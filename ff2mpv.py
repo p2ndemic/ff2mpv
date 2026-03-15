41c9a524ee9385e3fdcc6f1b9d1888b429234622
p2ndemic <162058065+p2ndemic@users.noreply.github.com>
2026-03-15 05:05:10 +0500

    Update ff2mpv.py

--- ff2mpv.py
+++ ff2mpv.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python3
-
 import json
-import os
-import platform
 import struct
 import sys
 import subprocess
@@ -11,34 +8,20 @@ import subprocess
 def main():
     message = get_message()
     url = message.get("url")
-    options = message.get("options") or []
-
-    args = ["mpv", "--no-terminal", *options, "--", url]
-
-    kwargs = {}
-    # https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Native_messaging#Closing_the_native_app
-    if platform.system() == "Windows":
-        kwargs["creationflags"] = subprocess.CREATE_BREAKAWAY_FROM_JOB
-
-    # HACK(ww): On macOS, graphical applications inherit their path from `launchd`
-    # rather than the default path list in `/etc/paths`. `launchd` doesn't include
-    # Homebrew in its default list, which means that any installations
-    # of MPV and/or youtube-dl under that prefix aren't visible when spawning
-    # from, say, Firefox. The real fix is to modify `launchd.conf`, but that's
-    # invasive and maybe not what users want in the general case.
-    # Hence this nasty hack.
-    if platform.system() == "Darwin":
-        path = os.environ.get("PATH")
-        os.environ["PATH"] = f"/opt/homebrew/bin:/usr/local/bin:{path}"
-
-    subprocess.Popen(args, **kwargs)
-
-    # Need to respond something to avoid "Error: An unexpected error occurred"
-    # in Browser Console.
+    args = [
+        "systemd-run",
+        "--user", "--scope",
+        "--slice=app-graphical.slice",
+        "--collect",
+        "--no-block",
+        "--quiet",
+        "--",
+        "mpv", "--", url
+    ]
+    subprocess.Popen(args)
     send_message("ok")
 
 
-# https://developer.mozilla.org/en-US/Add-ons/WebExtensions/Native_messaging#App_side
 def get_message():
     raw_length = sys.stdin.buffer.read(4)
     if not raw_length:
@@ -49,10 +32,6 @@ def get_message():
 
 
 def send_message(message):
-    # https://stackoverflow.com/a/56563264
-    # https://docs.python.org/3/library/json.html#basic-usage
-    # To get the most compact JSON representation, you should specify
-    # (',', ':') to eliminate whitespace.
     content = json.dumps(message, separators=(",", ":")).encode("utf-8")
     length = struct.pack("@I", len(content))
     sys.stdout.buffer.write(length)
